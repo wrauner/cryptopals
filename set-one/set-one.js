4fca4cfb14f2dbb218bce73a9db1501d6ba0e4cf
wojciechrauner <wojciech.rauner@gmail.com>
2017-03-01 18:32:48 +0100

    S1C2 xoring

--- set-one/set-one.js
+++ set-one/set-one.js
@@ -8,7 +8,16 @@ const SetOne = {
    * @param {string} data hex encoded string
    * @return {string} data converted to base64 format
    */
-  hex2base64: (data) => Buffer.from(data, 'hex').toString('base64')
+  hex2base64: (data) => Buffer.from(data, 'hex').toString('base64'),
+
+  xor: (a, b) => {
+    let length = (a.length > b.length) ? b.length : a.length
+    let result = Buffer.alloc(length)
+    for(let i=0; i<length; i++) {
+      result[i] = a[i] ^ b[i]
+    }
+    return result
+  }
 }
 
 module.exports = SetOne
--- set-one/set-one.spec.js
+++ set-one/set-one.spec.js
@@ -3,12 +3,20 @@ const setOneModule = require('./set-one')
 
 describe('Set one challenges', () => {
   describe('Set one module', () => {
-    it('it converts hex to base64', () => {
+    it('converts hex to base64', () => {
       let test = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
       let result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
       let convertedResult = setOneModule.hex2base64(test)
-      expect(convertedResult).to.equal(result);
+      expect(convertedResult).to.equal(result)
+    })
+    it('xors two strings', () => {
+      let test = Buffer.from('1c0111001f010100061a024b53535009181c', 'hex')
+      let key = Buffer.from('686974207468652062756c6c277320657965', 'hex')
+      let result = Buffer.from('746865206b696420646f6e277420706c6179', 'hex')
+
+      let xoredResult = setOneModule.xor(test, key)
+      expect(result).to.deep.equal(xoredResult)
     })
   })
 })
