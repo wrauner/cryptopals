be838abd13fd85ba61c0f51ac9f8e7c60a58e81b
wojciechrauner <wojciech.rauner@gmail.com>
2017-03-02 18:19:43 +0100

    repeating-key xor

--- set-one/set-one.test.js
+++ set-one/set-one.test.js
@@ -36,12 +36,22 @@ describe('Set 1, Basic', () => {
       let test = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
       let result = xorBreaker.breakSingleXor(test)
       mlog.log(JSON.stringify(result[1], undefined, 2))
+      expect(result[1]).to.equal("Cooking MC's like a pound of bacon")
     })
   })
   describe('Challenge 4, Detect single-character XOR', () => {
     it('detects and breaks single xor', () => {
       let result = xorBreaker.breakSingleXorFile('./set-one/data/4.txt')
       mlog.log(JSON.stringify(result[1], undefined, 2))
+      expect(result[1]).to.equal("Now that the party is jumping\n")
+    })
+  })
+  describe('Challenge 5, Implement repeating-key XOR', () => {
+    it('Xors repeating key', () => {
+      let verse_one = Buffer.from("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+      let result = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+      let xor_one = xorUtils.xor(verse_one, Buffer.from('ICE')).toString('hex')
+      expect(xor_one).to.equal(result)
     })
   })
 })
